2cd9f32a27ab60e25e0cefd9a9b9f21b2d3f694f
ThugLife13 <127151276+ThugLife13@users.noreply.github.com>
2024-10-09 10:28:06 +0200

    Initial commit

---
+++ lib/core.h
@@ -0,0 +1,16 @@
+//
+// Created by mateu on 11.09.2024.
+//
+
+#ifndef CORE_H
+#define CORE_H
+
+
+
+class core {
+
+};
+
+
+
+#endif //CORE_H
---
+++ src/core.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mateu on 11.09.2024.
+//
+
+#include "../lib/core.h"
