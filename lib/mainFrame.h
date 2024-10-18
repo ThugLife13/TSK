765761ba63cf0e91e93fa16a95fb38e690335a69
ThugLife13 <127151276+ThugLife13@users.noreply.github.com>
2024-10-18 12:17:05 +0200

    basic handlers methodes

--- lib/mainFrame.h
+++ lib/mainFrame.h
@@ -8,6 +8,12 @@ class mainFrame : public wxFrame {
 public:
     mainFrame(const wxString &title);
     ~mainFrame();
+private:
+    void startButtonHandler(wxCommandEvent& evn);
+    void newWindowCheckBoxHandler(wxCommandEvent& env);
+    void radarPosXSliderHandler(wxCommandEvent& env);
+    void radarPosYSliderHandler(wxCommandEvent& env);
+    void carSpeedControllerHandler(wxCommandEvent& env);
 };
 
 
--- src/mainFrame.cpp
+++ src/mainFrame.cpp
@@ -2,18 +2,58 @@
 
 mainFrame::mainFrame(const wxString &title): wxFrame(nullptr, wxID_ANY, title, wxPoint(10,10), wxSize(1920, 1080)) {
     wxPanel *mainPanel = new wxPanel(this, wxID_ANY);
-    wxButton* firstButton = new wxButton(mainPanel, wxID_ANY, wxT("First"), wxPoint(100, 100), wxSize(100, -1));
 
-    wxCheckBox* firstCheck = new wxCheckBox(mainPanel, wxID_ANY, wxT("Check"), wxPoint(100, 200), wxSize(100, -1));
+    //MAIN WINDOW
+    //start sim button
+    wxButton* startButton = new wxButton(mainPanel, wxID_ANY, wxT("Start Sim"), wxPoint(100, 100), wxSize(100, -1));
 
+    //track in new window checkBox
+    wxCheckBox* newWindowCheckBox = new wxCheckBox(mainPanel, wxID_ANY, wxT("Track sim in new window"), wxPoint(100, 200), wxSize(100, -1));
+
+    //radar position controller
+    wxSlider* radarPosXSlider = new wxSlider(mainPanel, wxID_ANY, 10, 1, 100, wxPoint(100, 500), wxSize(100, -1));
+    wxSlider* radarPosYSlider = new wxSlider(mainPanel, wxID_ANY, 10, 1, 100, wxPoint(100, 600), wxSize(100, -1));
+
+    //speed controller
+    wxSpinCtrl* speedSpinController = new wxSpinCtrl(mainPanel, wxID_ANY, wxT("Car true speed"), wxPoint(100, 700), wxSize(100, -1));
+
+
+    //static text
     wxStaticText* firstText = new wxStaticText(mainPanel, wxID_ANY, wxT("First text - static"), wxPoint(100, 300), wxSize(100, -1));
 
-    wxTextCtrl* firstTextCtrl = new wxTextCtrl(mainPanel, wxID_ANY, wxT("First text"), wxPoint(100, 400), wxSize(100, -1));
+    //handlers
+    startButton->Bind(wxEVT_BUTTON, &mainFrame::startButtonHandler, this);
+    newWindowCheckBox->Bind(wxEVT_CHECKBOX, &mainFrame::newWindowCheckBoxHandler, this);
+    radarPosXSlider->Bind(wxEVT_SLIDER, &mainFrame::radarPosXSliderHandler, this);
+    radarPosYSlider->Bind(wxEVT_SLIDER, &mainFrame::radarPosYSliderHandler, this);
+    speedSpinController->Bind(wxEVT_SPINCTRL, &mainFrame::carSpeedControllerHandler, this);
 
-    wxSlider* firstSlider = new wxSlider(mainPanel, wxID_ANY, 10, 1, 100, wxPoint(100, 500), wxSize(100, -1));
-
-    wxSpinCtrl* firstSpin = new wxSpinCtrl(mainPanel, wxID_ANY, wxT("First number controller"), wxPoint(100, 600), wxSize(100, -1));
 }
 
 mainFrame::~mainFrame() {
 }
+
+void mainFrame::startButtonHandler(wxCommandEvent& evn) {
+    wxMessageBox("Button Clicked");
+}
+
+void mainFrame::newWindowCheckBoxHandler(wxCommandEvent& env) {
+    wxMessageBox("CheckBox Clicked");
+
+}
+
+void mainFrame::radarPosXSliderHandler(wxCommandEvent& env) {
+    //wxMessageBox("Slider X %d" + env.GetInt());
+
+}
+
+void mainFrame::radarPosYSliderHandler(wxCommandEvent& env) {
+    //wxMessageBox("Slider Y %d" + env.GetInt());
+
+}
+
+void mainFrame::carSpeedControllerHandler(wxCommandEvent& env) {
+    //wxMessageBox("Spin Controller X %d" + env.GetInt());
+
+}
+
